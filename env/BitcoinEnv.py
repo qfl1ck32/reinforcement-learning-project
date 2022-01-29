c45041ddb9bd49442f5cbe8f9a5e7cda56d54ee2
mousepad01 <acalin2000@gmail.com>
2022-01-29 13:33:30 +0200

    rendering added

--- env/BitcoinEnv.py
+++ env/BitcoinEnv.py
@@ -1,12 +1,24 @@
 import numpy as np
 import tensorflow as tf
 
+from matplotlib import pyplot as plt
+
 from gym import Env
 from gym.spaces import Box
 
 import random
 
+from logger import logger
+
 class BitcoinTradingEnv(Env):
+
+    MIN_BALANCE = 100
+    """Threshold for stopping an episode"""
+
+    # render strings
+    TOTAL_BALANCE_STR = "Total balance"
+    CURRENCY_STR = "Currency"
+    BTC_STR = "BTC (in currency)"
 
     def __init__(self, 
                     price_history,
@@ -14,19 +26,26 @@ class BitcoinTradingEnv(Env):
                     start_btc = 0.0,
                     memory = 5,
                     episode_len = 5000,
-                    relative_reward = True,
+                    stats4render = True,
                 ):
         super(BitcoinTradingEnv, self).__init__()
 
         assert(memory >= 1)
         assert(start_btc >= 0.0)
         assert(start_money >= 0)
+        assert(episode_len > memory)
 
+        self.stats4render = stats4render
+        """Must be true to be able to call .render(),
+            but it consumes more memory"""
+
+        if self.stats4render:
+            self.balance_money_btc = []
+            """List of tuples (total balance, money, btc)"""
+
+        self.steps_todo = episode_len
         self.episode_len = episode_len
-        self.steps_todo = self.episode_len
-
-        self.relative_reward = relative_reward
-        """parameter that slightly tweaks reward mechanism in some cases"""
+        """One episode length, in data entries"""
 
         self.money = 0
         self.start_money = start_money
@@ -41,11 +60,12 @@ class BitcoinTradingEnv(Env):
         """price over last <<self.memory>> entries"""
 
         self.last_observation = None
+        """last observation state"""
 
         self.action_space = Box(low=-1.0, high=1.0, dtype=np.float32, shape=(1,))
         """between [-1, 1]\n
-            eg. +0.7 means 'spend 70% of money that I have right now to buy btc'\n
-                -0.5 means 'sell 50% of the btc I currently have'"""
+            eg. +0.7 means spend 70% of money that I have right now to buy btc\n
+                -0.5 means sell 50% of the btc I currently have"""
         
         # could've designed a lazy loader or related
         # but it's easier, safer, and faster to just map all data in memory
@@ -64,8 +84,8 @@ class BitcoinTradingEnv(Env):
 
         self.current_moment = self.memory
         """clock, starting from 0\n
-            eg. self.memory = 3 =>\n
-            current moment = 3, (initial) last observation = [p1 / p0, p2 / p1, p3 / p2]"""
+            eg. self.memory = 3 =>
+            current moment = 3, (initial)\n last observation = [p1 / p0, p2 / p1, p3 / p2]"""
 
     def reset(self):
         
@@ -77,6 +97,9 @@ class BitcoinTradingEnv(Env):
 
         self.total_balance = self.start_money + self.start_btc * self.price_history[self.current_moment]
         self.steps_todo = self.episode_len
+
+        if self.stats4render:
+            self.balance_money_btc.append((self.total_balance, self.money, self.btc))
 
         return self.last_observation
 
@@ -91,14 +114,6 @@ class BitcoinTradingEnv(Env):
     # (observation, reward, done, info)
     def step(self, action):
 
-        def _compute_reward(r0, r1, r):
-
-            if (self.relative_reward is False) or (-1 <= (r0 - r1) <= 1):
-                return 2 * r
-
-            r_med = (r0 + r1) / 2
-            return r * (1 + (r - r_med) / ((r0 - r_med) if (r0 > r_med) else (r_med - r0)))
-
         observation = None
         reward = None
         info = {}   # unused
@@ -107,9 +122,6 @@ class BitcoinTradingEnv(Env):
         # if action < 0, sell btc with self.btc * (-action)
 
         current_price = self.price_history[self.current_moment]
-
-        r_sell_all = np.log((self.money + self.btc * self.price_history[self.current_moment + 1]) / self.total_balance)
-        r_buy_all = np.log(((self.btc + self.money / current_price) * self.price_history[self.current_moment + 1]) / self.total_balance)
 
         if action > 0:
             
@@ -121,15 +133,19 @@ class BitcoinTradingEnv(Env):
             self.money += self.btc * (-action) * current_price
             self.btc *= 1 + action
 
+        if self.total_balance < BitcoinTradingEnv.MIN_BALANCE:
+            return observation, -10000000000, False, info
+
         self.current_moment += 1
         observation = np.insert(self.last_observation[1:], self.memory - 1, self.price_history_deriv[self.current_moment - 1])
 
         new_total_balance = self.btc * self.price_history[self.current_moment] + self.money
-
         reward = np.log(new_total_balance / self.total_balance)
-        reward = _compute_reward(r_sell_all, r_buy_all, reward)   
 
         self.total_balance = new_total_balance
+
+        if self.stats4render:
+            self.balance_money_btc.append((self.total_balance, self.money, self.btc))
 
         self.steps_todo -= 1
         if (self.steps_todo == 0) or (self.current_moment == len(self.price_history) - 1):
@@ -138,7 +154,50 @@ class BitcoinTradingEnv(Env):
         return observation, reward, False, info
 
     def render(self, mode="human"):
-        print("not implemented, sorry :P")
+        """Should contain:\n
+            * A graph plotting all prices in the episode_len range\n
+            * A graph plotting total balance, and subgraphs for btc and money\n"""
+
+        if mode != "human":
+            logger.warn("not implemented, sorry :P")
+            return
+
+        if self.stats4render is False:
+            logger.warn("Must have stats4render set to true at the construction of the env to be able to show stats!")
+            return 
+
+        start_moment = self.current_moment + self.steps_todo - self.episode_len
+        end_moment = start_moment + self.episode_len
+
+        fig, (price_time, balance_time) = plt.subplots(2)
+        fig.subplots_adjust(hspace=0.5)
+
+        price_time.plot(range(start_moment, end_moment + 1),
+                        self.price_history[start_moment: end_moment + 1],
+                        color = 'gold')
+
+        price_time.set_title("Price evolution")
+        price_time.set_xlabel("time")
+        price_time.set_ylabel("$ / BTC")
+        price_time.grid(True)
+
+        balance_stats = {"Currency": [float(t[1]) for t in self.balance_money_btc],
+                        "BTC (currency equivalent)": [float(t[0] - t[1]) for t in self.balance_money_btc]}
+        
+
+        balance_time.stackplot(range(start_moment, end_moment + 1),
+                                balance_stats.values(),
+                                labels = balance_stats.keys(),
+                                alpha = 0.8)
+
+        balance_time.legend(loc = "upper left")
+        balance_time.set_title("Balance evolution")
+        balance_time.set_xlabel("time")
+        balance_time.set_ylabel("$")
+        balance_time.grid(True)
+
+        plt.title(f"Bitcoin trading environment (episode length {self.episode_len})")
+        plt.show()
 
     def close(self):
         return None
